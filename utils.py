a4e3f30a58e714bc644185df77fd60e6fb0a17e0
adarshkumar714 <123639529+adarshkumar714@users.noreply.github.com>
2023-04-12 00:13:20 +0530

    fixed some errors

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 import openai
 import requests
 import json
+import os
+
+openai.api_key = os.getenv('OPENAI_KEY')
+
 
 def get_quote():
     try:
@@ -28,7 +32,7 @@ def chat_gpt_get(content):
                 {"role": "user", "content": content}
             ]
         )["choices"][0]["message"]["content"] 
-        return reponse
+        return response
     except Exception as e:
         print("[!] Failed to get Chat-GPT reponse, need to fix it")
         print(e)
